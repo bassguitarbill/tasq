b3315f6a7350f0cda89ab093c0d110cb91e6e230
Bill <bassguitarbill@gmail.com>
2015-10-15 22:11:27 -0400

    Add promises and an insert function

--- app.js
+++ app.js
@@ -4,16 +4,19 @@ var ObjectId = mongo.ObjectID;
 
 var assert = require('assert');
 
+var Promise = require('promise');
+
 var url = 'mongodb://localhost:27017/test';
 
 function findTasks(db, callback) {
-   var cursor =db.collection('tasks').find( );
+   var cursor = db.collection('tasks').find( );
+   var returnList = [];
    cursor.each(function(err, doc) {
       assert.equal(err, null);
       if (doc != null) {
-         console.dir(doc);
+         returnList.push(doc);
       } else {
-         callback();
+         callback(returnList);
       }
    });
 };
@@ -21,8 +24,59 @@ function findTasks(db, callback) {
 function execTask(task) {
 	Client.connect(url, function(err, db) {
 		assert.equal(null, err);
-		task(db, function(){db.close()});
+		task(db, function(returnData){console.log(returnData);db.close()});
 	});
 }
 
-execTask(findTasks); 
+function query(collName, query, callback) {
+	Client.connect(url, function(err, db) {
+		assert.equal(null, err);
+		var cursor = db.collection(collName).find(query);
+		var returnList = [];
+		cursor.each(function(err, doc) {
+			assert.equal(err, null);
+			if (doc != null) {
+				returnList.push(doc);
+			} else {
+				callback(returnList); 
+				db.close();
+			}
+		});
+	});
+};
+
+function getQueryPromise(collName, query) {
+	return new Promise(function(resolve, reject){
+		Client.connect(url, function(err, db) {
+			if(err != null)
+				reject(err);
+			assert.equal(err, null);
+			var cursor = db.collection(collName).find(query);
+			var returnList = [];
+			cursor.each(function(err, doc) {
+				if(err != null)
+					reject(err);
+				assert.equal(err, null);
+				if (doc != null) {
+					returnList.push(doc);
+				} else {
+					db.close();
+					resolve(returnList); 
+				}
+			});
+		});
+	});
+}
+
+getQueryPromise("tasks", {}).then(function(data){console.log(data);});
+module.exports.getQueryPromise = getQueryPromise;
+
+function insert(collName, document, callback) {
+	Client.connect(url, function(err, db) {
+		assert.equal(null, err);
+		db.collection(collName).insertOne(
+			document,
+			function(err, result){db.close();callback(result)}
+		);
+	});
+}
