3b2bfe71961ba28c8d07549092295d682d6255c5
Bill <bassguitarbill@gmail.com>
2015-10-17 18:19:48 -0400

    Add task-inputting capabilities

--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,54 @@
 $(document).ready(function(){
 
-	getActivities()
+	init();
 
 });
 
-function getActivities(){
-	$.get('/activities', function(data){
-		console.log(data);
-		data.forEach(function(act){
-			$("body").append('<div style="background-color:' + act.color + '"><p class="name">' + act.name + '</p><p class="desc">' + act.desc + '</p></div>');
+function init() {
+	getActivities_p()
+	.then(addActivities);
+}
+
+function getActivities_p() {
+	return new Promise(function(resolve, reject){
+		$.ajax({
+			dataType:"json",
+			error:reject,
+			method:'GET',
+			success:resolve,
+			url:'/activities'
+		});
+	});
+}
+
+function addActivities(activityList) {
+	activityList.forEach(function(act){
+		$("#taskList").append('<div style="background-color:' + act.color + '"><p class="name">' + act.name + '</p><p class="desc">' + act.desc + '</p></div>');
+		$(".activitySelector").append('<option>' + act.name + '</option>');
+	});
+	return activityList;
+}
+
+function addTask() {
+	addTask_p()
+	.then(d => console.log(d))
+	.catch(e => console.err(e))
+}
+
+function addTask_p() {
+	var task = {
+		activity: $('#newActivitySelector>option:selected').text(),
+		start: new Date(),
+		goal: $('#newActivity>.goal').val()
+	}
+	return new Promise(function(resolve, reject){
+		$.ajax({
+			data:task,
+			dataType:"json",
+			error:reject,
+			method:'POST',
+			success:resolve,
+			url:'/log'
 		});
 	});
 }
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+var bodyParser = require('body-parser')
+
 var db = require('./db');
 
 var PORT = 8080;
@@ -8,8 +10,27 @@ var PORT = 8080;
 app.use(express.static('public'));
 
 app.get('/activities', function (req, res) {
-  db.getQueryPromise('activity',{})
-	.then(function(d){res.send(d).end();});
+	db.getQueryPromise('activity',{})
+		.then(function(d){res.send(d).end();});
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.post('/log', function (req, res) {
+	console.log(req.body);
+	if(!req.body || !req.body.activity){
+		res.statusCode = 400;
+		res.send('No activity provided').end();
+	} else {
+		var log = {
+			activity: req.body.activity,
+			start: req.body.start || new Date(),
+			stop: req.body.stop || null,
+			goal: req.body.goal || "",
+			journal: req.body.journal || null
+		};
+		db.getInsertPromise('task',req.body)
+			.then(function(d){res.send(d).end();});
+	}
 });
  
 app.listen(PORT);
